5dfed4d3a891a423afe327f0f9bf1aebdab18104
Wilmar Florez <wflorez52@gmail.com>
2019-02-20 09:30:21 -0500

    Incorporando Stylus y Pug

---
+++ platzi-music/src/App.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    h1 {{ msg }}
+    h2 Este es mi título
+    ul
+      li
+        a(href='https://vuejs.org', target='_blank') Core Docs
+      li
+        a(href='https://forum.vuejs.org', target='_blank') Forum
+      li
+        a(href='https://chat.vuejs.org', target='_blank') Community Chat
+      li
+        a(href='https://twitter.com/vuejs', target='_blank') Twitter
+    h2 Ecosystem
+    ul
+      li
+        a(href='http://router.vuejs.org/', target='_blank') vue-router
+      li
+        a(href='http://vuex.vuejs.org/', target='_blank') vuex
+      li
+        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
+      li
+        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color red
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+</style>
